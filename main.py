b2d9feeb42ed8b8586efa6a7e4af48d6e6960028
antenna <nanesterenko>
2022-03-03 23:08:56 +0300

    final variant

--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 
 def passing_matrix(my_input: list, x: int, y: int, col: int, row: int) -> None:
     """Обход матрицы."""
-    if x < 0 or x > row or y < 0 or y > col:
-        print(f"Значения вне диапазона: x = {x}, y = {y}")
-        # выходить ли из программы, если следующее действие вызовет исключение?
+    if x < 0 or x >= row or y < 0 or y >= col:
+        return None
 
     if my_input[x][y] == 0:
         return None
@@ -14,17 +13,10 @@ def passing_matrix(my_input: list, x: int, y: int, col: int, row: int) -> None:
     my_input[x][y] = 0
 
     """Сдвигаемся  вверх, вниз, вправо, влево для проверки кратера"""
-
-
-    if x > 0:
-        passing_matrix(my_input, x - 1, y, col, row)
-    if x > row - 1:
-        passing_matrix(my_input, x + 1, y, col, row)
-    if y > 0:
-        passing_matrix(my_input, x, y - 1, col, row)
-    if y > col - 1:
-        passing_matrix(my_input, x, y + 1, col, row)
-
+    passing_matrix(my_input, x - 1, y, col, row)
+    passing_matrix(my_input, x + 1, y, col, row)
+    passing_matrix(my_input, x, y - 1, col, row)
+    passing_matrix(my_input, x, y + 1, col, row)
 
 
 def calculate(my_input: list) -> int:
@@ -39,6 +31,7 @@ def calculate(my_input: list) -> int:
         for j in range(col):
             if my_input[i][j] == 1:
                 passing_matrix(my_input, i, j, col, row)
+                print(my_input)
                 count += 1
     return count
 
@@ -48,13 +41,18 @@ def read_from_file(path_file: str) -> list:
     with open(path_file, 'r') as file:
         massiv = []
         for line in file:
-            num = list(map(int, line.split()))  # как обрабатывать что не можем преобразовать?
+            num = list(map(int, line.split()))
             massiv.append(num)
         if len(massiv) == 0:
             print("Не удалось прочитать массив")
         return massiv
 
 
-my_input = read_from_file('file.txt')
-count_craters = calculate(my_input)
-print(count_craters)
+def main(path_file: str) -> None:
+    my_input = read_from_file(path_file)
+    count_craters = calculate(my_input)
+    print("Число кратеров: ", count_craters)
+
+
+if __name__ == '__main__':
+    main('file.txt')
